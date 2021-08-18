67b9e46f4bfc8d95988e68764b6e65eb296207ec
= <=>
2021-08-19 02:02:21 +0600

    team to footer

--- src/StartupMain/Members/Member.js
+++ src/StartupMain/Members/Member.js
@@ -1,103 +1,92 @@
-import React from 'react'
-import {Grid,Typography,Divider} from '@material-ui/core';
-import {makeStyles} from '@material-ui/core/styles';
-import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import PhoneSharpIcon from '@material-ui/icons/PhoneSharp';
+import React from "react";
+import { Grid, Typography, Divider, Link } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import PhoneSharpIcon from "@material-ui/icons/PhoneSharp";
 
-const useStyles = makeStyles(()=>({
-    imgStyle:{
-        objectFit:"contain",
-        height:"100%",
-        borderRadius:".5rem",
-        width:"100%",
+const useStyles = makeStyles(() => ({
+  imgStyle: {
+    objectFit: "contain",
+    height: "100%",
+    borderRadius: ".5rem",
+    width: "100%",
+  },
+  workFigure: {
+    position: "relative",
+    textAlign: "center",
+    color: "#fff",
+    overflow: "hidden",
+    cursor: "pointer",
+    margin: "0",
+    "&:hover figcaption": {
+      transform: "translateY(0%)",
+      visibility: "visible",
+      opacity: 1,
     },
-    workFigure:{
-        position:"relative",
-        textAlign:"center",
-        color:"#fff",
-        overflow:"hidden",
-        cursor:"pointer",
-        margin:"0",
-        '&:hover figcaption':{
-            transform: 'translateY(0%)',
-            visibility:'visible',
-            opacity:1,
-        }
-    },
-    teamFig:{
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center",
-    },
-    workCaption:{
-        position:"absolute",
-        top:'0',
-        left:'0',
-        transform:'translateY(50%)',
-        width:'100%',
-        height:'100%',
-        background:'#17a2bb',
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center",
-        alignContent:"center",
-        transition:'all .5s',
-        opacity:0,
-        visibility:'hidden',
-       
-    },
-    attachClip:{
-        height:'1.5rem',
-        width:'1.5rem',
-        borderRadius:'3px',
-        padding:'3px',
-        transform:'rotate(45deg)',
-        color:"#ac89ea",
-        position:"absolute",
-        display:"flex",
-        justifyContent:'center',
-        alignItems:'center',
-        '&:hover':{
-            color:'#fff',
-            backgroundColor:'#000 !important',
-            zIndex:'10',
-        }
-        },
-    logoStyle:{
-        width:"16px",
-        height:"16px",
-        transform:'rotate(-45deg)',
-    },
-    memName:{
-        display:"inline-block",
-        background:"#17a2b8",
-        width:'80%',
-        margin:'0 auto',
-        color:"#fff",
-        padding:'.4rem 1rem',
-        borderRadius:'.2rem',
-        transform:'translateY(-50%)',
-        zIndex:"100",
-        fontSize:"13px"
-    },
-    contact:{
-        display:"flex",
-        justifyContent:"space-between",
-        margin:"5px 25px",
-        padding:"3px",
-        color:"grey",
-        fontSize:"13px",
-    }
-
+  },
+  teamFig: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  workCaption: {
+    position: "absolute",
+    top: "0",
+    left: "0",
+    transform: "translateY(50%)",
+    width: "100%",
+    height: "100%",
+    background: "#17a2bb",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    transition: "all .5s",
+    opacity: 0,
+    visibility: "hidden",
+  },
+  logoStyle: {
+    width: "18px",
+    height: "18px",
+    cursor: "pointer",
+  },
+  memName: {
+    display: "inline-block",
+    background: "#17a2b8",
+    width: "80%",
+    margin: "0 auto",
+    color: "#fff",
+    padding: ".4rem 1rem",
+    borderRadius: ".2rem",
+    transform: "translateY(-50%)",
+    zIndex: "100",
+    fontSize: "15px",
+    fontFamily:"cursive",
+  },
+  contact: {
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    margin: "5px",
+    padding: "5px",
+    color: "grey",
+    fontSize: "13px",
+  },
+  socials: {
+    justifyContent: "space-around",
+    alignItems: "center",
+    padding: "5px",
+  },
 }));
 
-function Member({name,img,position,gmail,phone}) {
-    const classes = useStyles();
-    return (
-        <>
-        <figure className={classes.workFigure}>
-            <img src={img} alt={name} className={classes.imgStyle} />
-            <figcaption className={classes.workCaption}>
+function Member({ name, img, position, gmail, phone }) {
+  const classes = useStyles();
+
+  return (
+    <>
+      <figure className={classes.workFigure}>
+        <img src={img} alt={name} className={classes.imgStyle} />
+        {/* <figcaption className={classes.workCaption}>
                 <Grid item xs={12} className={classes.teamFig}>
                     <div className={classes.attachClip} style={{ background:"#fff",marginLeft:"-90px"}}>
                         <img src="./img/facebook.png" alt="test" className={classes.logoStyle}/>
@@ -120,23 +109,52 @@ function Member({name,img,position,gmail,phone}) {
                     
                 </Grid>
                    
-            </figcaption>
-        </figure>
-            <Typography variant="body1" className={classes.memName}>{name}</Typography>
-            <Typography variant="body1" style={{ fontSize:"13px",marginBottom:"18px"}}>{position}</Typography>
-            <Divider />
-            <Grid className={classes.contact}>
-                <MailOutlineIcon style={{ fontSize:"18px"}}/>
-                <Typography variant="body1" style={{ fontSize:"13px"}}>{gmail}</Typography>
-            </Grid>
-            <Grid className={classes.contact}>
-                <PhoneSharpIcon style={{ fontSize:"18px"}}/>
-                <Typography variant="body1" style={{ fontSize:"13px"}}>{phone}</Typography>
-            </Grid>
-            
-       
-        </>
-    )
+            </figcaption> */}
+      </figure>
+
+      <Typography variant="body1" className={classes.memName}>
+        {name}
+      </Typography>
+      <Typography
+        variant="body1"
+        style={{ fontSize: "14px", marginBottom: "12px",fontFamily:"cursive", }}
+      >
+        {position}
+      </Typography>
+
+      <Divider />
+
+      <Grid className={classes.contact}>
+        <MailOutlineIcon style={{ fontSize: "18px" }} />
+        <Typography variant="body1" style={{ fontSize: "13px" }}>
+          {gmail}
+        </Typography>
+      </Grid>
+      <Grid className={classes.contact}>
+        <PhoneSharpIcon style={{ fontSize: "18px" }} />
+        <Typography variant="body1" style={{ fontSize: "13px" }}>
+          {phone}
+        </Typography>
+      </Grid>
+      <Divider />
+      <Grid container className={classes.socials}>
+        <RenderSocial imgUrl="./img/facebook.png" url="https://www.facebook.com/asif.mahmud.92317" text="fb" />
+        <RenderSocial imgUrl="./img/linkedin.png" url="#" text="linkedin" />
+        <RenderSocial imgUrl="./img/twitter.png" url="#" text="goggle" />
+      </Grid>
+    </>
+  );
 }
 
-export default Member
+export const RenderSocial = ({ imgUrl, url, text }) => {
+  const classes = useStyles();
+  return (
+    <Grid item>
+      <Link href={url}>
+        <img src={imgUrl} alt={text} className={classes.logoStyle} />
+      </Link>
+    </Grid>
+  );
+};
+
+export default Member;
--- src/StartupMain/Members/TeamMembers.js
+++ src/StartupMain/Members/TeamMembers.js
@@ -1,48 +1,68 @@
-import React from 'react'
-import {Grid,Typography} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Member from './Member';
+import React from "react";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import SectionHeader from "../Common/SectionHeader";
+import Member from "./Member";
 
-const useStyles = makeStyles(()=>({
-    root:{
-        minHeight:"420px",
-        marginTop:"3rem",
-        textAlign:"center"
-    },
-    sampleWork:{
-        display:'flex',
-        flexWrap:"wrap",
-        justifyContent:"center",
-
-    },
-    sampleItem:{
-        display:"flex",
-        margin:'5px',
-        borderRadius:".25rem",
-        border:"1px solid #dee2e6",
-        flexDirection:"column",
-
-    },
+const useStyles = makeStyles(() => ({
+  root: {
+    minHeight: "420px",
+    marginTop: "3rem",
+    textAlign: "center",
+  },
+  sampleWork: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+    margin: "0 30px",
+  },
+  sampleItem: {
+    display: "flex",
+    margin: "5px",
+    borderRadius: ".25rem",
+    border: "1px solid #dee2e6",
+    flexDirection: "column",
+  },
 }));
-function TeamMembers({members}) {
-    const classes = useStyles();
-    return (
-        <Grid container justify="center" alignItems="center" className={classes.root}>
-             <Grid item style={{ marginBottom:"3rem"}}>
-                <Typography variant="h4"> <span className="text-info">Team</span> Members</Typography>
-                <Typography variant="body2" style={{color:"#6c757d"}} >We’re some creative people with powerful knowledge & awesome skills behind the scene bringing you the bests</Typography>
-
-            </Grid>
-            <Grid className={classes.sampleWork}>
-            {members.map((item,ind)=>
-                <Grid item key={ind} md={2} sm={3} xs={6} className={classes.sampleItem}>
-                <Member name={item.name} img={item.img} position={item.position} gmail={item.gmail} phone={item.phone}/>
-                </Grid>               
-                )}
-                
-            </Grid>
-        </Grid>
-    )
+function TeamMembers({ members }) {
+  const classes = useStyles();
+  return (
+    <Grid
+      container
+      justify="center"
+      alignItems="center"
+      className={classes.root}
+    >
+      <Grid item style={{ marginBottom: "3rem" }}>
+        <SectionHeader
+          isWhite={true}
+          text="Team"
+          colorText="Members"
+          description="We’re some creative people with powerful knowledge & awesome skills behind the scene bringing you the bests"
+        />
+      </Grid>
+      <Grid className={classes.sampleWork}>
+        {members.map((item, ind) => (
+          <Grid
+            item
+            key={ind}
+            md={2}
+            // sm={4}
+            xs={6}
+            className={classes.sampleItem}
+          >
+            <Member
+              name={item.name}
+              img={item.img}
+              position={item.position}
+              gmail={item.gmail}
+              phone={item.phone}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Grid>
+  );
 }
 
-export default TeamMembers
+export default TeamMembers;
